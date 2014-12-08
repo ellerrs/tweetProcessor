0033b4f2ab39127ada085e9d7e228e6c31f5829a
Shane Eller <shane.eller@gmail.com>
2014-12-08 21:50:07 +0000

    moving the ngramr to a service that waits for tweets to process

--- chattersum/chattersum.py
+++ chattersum/chattersum.py
@@ -7,16 +7,28 @@ import os
 import signal
 import sys
 
-import ngramr
-
 
 def usage():
-    print " -h, --help  Show this"
+    print "usage: ./chattersum.py [option] [arg] ..."
+    print "Options and arguments (and corresponding environment variables):"
+    print ""
+    print "-d, --dump  hour     : create a tar.gz archive of the raw tweets from <hour>s ago. example: --dump 1"
+    print "-h, --help           : show this"
+    print "-s, --start service  : start the requested service. example: --start streamr"
+    print "-S, --stop  service  : stop the requested service. example: --stop streamr"
+    print "-v, --verbose        : crank verbosity to maximum. (this if funny if you've played Zork)"
+    
+
+
+
+def getHour(hours_ago):
+    timedif     = datetime.datetime.now() - datetime.timedelta(hours=hours_ago)
+    return timedif.strftime('%Y%m%d%H')
 
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "vhsS", ["verbose", "help", "start", "stop"])  # :v to add arg
+        opts, args = getopt.getopt(sys.argv[1:], "vhs:S:d:", ["verbose", "help", "start=", "stop=", "dump="])  # :v to add arg
     except getopt.GetoptError as err:
         print str(err) 
         usage()
@@ -37,23 +49,52 @@ def main():
             sys.exit()
         
         elif o in ("-s", "--start"):
-            from streamr import start as streamrStart
-            pid = os.fork()
-            if (pid == 0):          # The first child.
-                os.chdir("/")
-                os.setsid()
-                os.umask(0) 
-                pid2 = os.fork() 
-                if (pid2 == 0):     # Second child
-                    streamrStart()
-                else:
-                    sys.exit()      #First child
-            else:                   # Parent Code
-                sys.exit()          # Parent exists
+
+            if a == "streamr":
+                from streamr import start as streamrStart
+                pid = os.fork()
+                if (pid == 0):          # The first child.
+                    os.chdir("/")
+                    os.setsid()
+                    os.umask(0) 
+                    pid2 = os.fork() 
+                    if (pid2 == 0):     # Second child
+                        streamrStart()
+                    else:
+                        sys.exit()      #First child
+                else:                   # Parent Code
+                    sys.exit()          # Parent exists
+            elif a == "ngramr":
+                from ngramr import start as ngramrStart
+                pid = os.fork()
+                if (pid == 0):          # The first child.
+                    os.chdir("/")
+                    os.setsid()
+                    os.umask(0) 
+                    pid2 = os.fork() 
+                    if (pid2 == 0):     # Second child
+                        ngramrStart()
+                    else:
+                        sys.exit()      #First child
+                else:                   # Parent Code
+                    sys.exit()
+            else:
+                usage()
+                sys.exit()
+
         
         elif o in ("-S", "--stop"):
-            from streamr import stop as streamrStop
-            streamrStop()
+            if a == "streamr":
+                from streamr import stop as streamrStop
+                streamrStop()
+            elif a == 'ngramr':
+                from ngramr import stop as ngramrStop
+                ngramrStop()
+
+        elif o in ("-d", "--dump"):
+            #from ngramr import buildGramsBucket
+            #buildGramsBucket(getHour(a))
+            print('dump')
         
         else:
             assert False, "unhandled option"
--- chattersum/ngramr.py
+++ chattersum/ngramr.py
@@ -4,11 +4,14 @@
 import config
 import datetime
 import getopt
+import logging
 import nltk
 from nltk.corpus import stopwords
 from nltk.util import ngrams
 import operator
+import os
 from pymongo import MongoClient
+import signal
 import sys
 import time
 import zc.lockfile
@@ -17,6 +20,9 @@ import zc.lockfile
 client = MongoClient( config.MONGO_HOST, config.MONGO_PORT)
 client.twitter.authenticate( config.MONGO_USER, config.MONGO_PASS, mechanism='MONGODB-CR')
 db = client.twitter
+
+updated     = 0
+inserted    = 0
 
 #words = nltk.word_tokenize(text)
 
@@ -28,32 +34,62 @@ def buildGramsStream(tweet):
     buildGrams(words, timestamp)
 
 
-def buildGramsBucket(hours_ago):
+def start():
     
     # Putting a lock file here to block hourly processing that has gone more than an hour
 
     lock = zc.lockfile.LockFile('/var/lock/ngramr')
     logging.info("ngramr: started")
 
-    timedif     = datetime.datetime.now() - datetime.timedelta(hours=hours_ago)
-    lasthour    = timedif.strftime('%Y%m%d%H')
-    logging.info("ngramr: working on hour %s" % lasthour)
-    
-    count = db.hose.find({'bucket': hours_ago}).count()
-    logging.info("ngramr: found %s tweets" % count)
+    #timedif     = datetime.datetime.now() - datetime.timedelta(hours=hours_ago)
+    #lasthour    = timedif.strftime('%Y%m%d%H')
+    #logging.info("ngramr: working on hour %s" % lasthour)
 
-    n = 1
-    total_to_process = count
+    max_to_process = 10000
+    while True:
+        count = db.hose.find({'processed': False}).count()
+        if count == 0 :
+            logging.info("ngramr: no tweets to process")
+        else:
+            logging.info("ngramr: found %s tweets to process. Working on the first %s" % (count, max_to_process))
 
-    for tweet in db.hose.find({'bucket': hours_ago}):
-        # throwing in a feedback loop so I can watch the log and see progress. 
-        if (n % 10000==0):
-            logging.info("ngramr: processed %s. only %s to go" % (n, (total_to_process - n)))  
+        n = 1
+
+        for tweet in db.hose.find({'processed': False}).limit(max_to_process):
+
+            # throwing in a feedback loop so I can watch the log and see progress. 
+            if (n % 100==0):
+                logging.debug("ngramr: processed %s. only %s to go" % (n, (count - n)))
             n = n + 1
-        text        = tweet['text'].encode('ascii','ignore').lower()
-        timestamp   = tweet['timestamp']
-        words = string.split(' ')
-        buildGrams(words, timestamp)
+
+            text        = tweet['text'].encode('ascii','ignore').lower()
+            timestamp   = tweet['timestamp']
+            words = text.split(' ')
+            
+            buildGrams(words, timestamp)
+            
+            db.hose.update({'_id': tweet['_id']},{'$set':{'processed':True}})
+        
+        logging.info("ngramr: sleeping for 60 seconds")
+        logging.info("ngramr: u:%s i:%s" % (updated, inserted))
+        time.sleep(60) 
+
+
+def stop():
+    
+    logging.info("streamr: attempting to stop ngramr")
+    
+    try:
+        f = open("/var/lock/ngramr", "r")
+        for line in f:
+            pid = line.strip()
+            os.kill(int(pid), signal.SIGKILL)
+            
+        os.remove('/var/lock/ngramr')
+        logging.info("ngramr: ngramr stopped")
+    
+    except Exception, e:
+        logging.error("ngramr: Exception: %s" % str(e))
 
 
 def buildGrams(words,timestamp):
@@ -93,5 +129,11 @@ def buildDistro(xgram, timestamp, gram_length):
                 }
             }
         }
-	db.grams.update(query, update, upsert=True)
+        r = db.grams.update(query, update, upsert=True)
+        if r['updatedExisting']:
+            global updated 
+            updated = updated + 1
+        else:
+            global inserted
+            inserted = inserted + 1
 
