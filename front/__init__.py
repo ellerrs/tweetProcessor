992bae35bb3fdd98f4949a0386f0f40e1b7a12fb
Shane Eller <shane.eller@gmail.com>
2015-06-11 16:06:40 -0400

    frontend support

--- front/__init__.py
+++ front/__init__.py
@@ -7,31 +7,31 @@ from flask.ext.login import (LoginManager, current_user, login_required,
 
 #from .utils import check_expired
 
-app = Flask(__name__)
-app.config.from_object('config')
+front = Flask(__name__)
+front.config.from_object('config')
 
-from app import utils
+from front import utils
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 login_manager.login_message = u"Please log in to access this page."
 login_manager.refresh_view = "reauth"
-login_manager.init_app(app)
+login_manager.init_app(front)
 
 # Define the database object which is imported
 # by modules and controllers
-db = SQLAlchemy(app)
+db = SQLAlchemy(front)
 
 # Sample HTTP error handling
-@app.errorhandler(404)
+@front.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
 
-from app.controllers.welcome import mod_welcome
-from app.controllers.auth import mod_auth
+from front.controllers.welcome import mod_welcome
+from front.controllers.auth import mod_auth
 
-app.register_blueprint(mod_welcome)
-app.register_blueprint(mod_auth)
+front.register_blueprint(mod_welcome)
+front.register_blueprint(mod_auth)
 
 db.create_all()
--- front/models/base.py
+++ front/models/base.py
@@ -1,4 +1,4 @@
-from app import db
+from front import db
 
 
 class Base(db.Model):
